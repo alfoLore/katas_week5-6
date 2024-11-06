3b66e1c4bd7a956041a7eeffc801e59a2284cf67
Lorena Febbraro <lorenafebbraro@gmail.com>
2024-11-06 16:11:25 -0300

    edit file convertir String to array

---
+++ convertirStringtoArray.js
@@ -0,0 +1,10 @@
+
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+//función que divida una cadena de texto y la convierta en un array de palabras.
+//Solucion :Esta función toma una cadena de texto como entrada y 
+//utiliza el método .split(" ") para dividirla en un array de palabras separadas por espacios.
+
+
+function stringToArray(string) {
+    return string.split(" ");
+}
