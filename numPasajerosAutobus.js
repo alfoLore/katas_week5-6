6bf9013d422f7407fc3f507f86a111917bbddb4e
Lorena Febbraro <lorenafebbraro@gmail.com>
2024-11-06 01:28:35 -0300

    Add description and kata link

--- numPasajerosAutobus.js
+++ numPasajerosAutobus.js
@@ -1,3 +1,9 @@
+//https://www.codewars.com/kata/5648b12ce68d9daa6b000099
+//Solucion: autobús está vacío en la primera parada, peopleOnBus = 0,
+//peopleIn (personas que suben) y peopleOut (personas que bajan) en el arreglo.La variable peopleOnBus
+//personas que quedan en el autobús después de la última parada.
+
+
 var number = function(busStops) {
     let peopleOnBus = 0;
   
