f4b944c8d76ee2eccecd76180da9ecc90c0552d3
Lorena Febbraro <lorenafebbraro@gmail.com>
2024-11-06 16:23:26 -0300

    kata return multiplicacion

---
+++ multiplicacion.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/50654ddff44f800200000004
+//cual es el error de esta funcion: function multiply(a, b){ 
+//    a * b
+//}
+//Solucion:agregarle el return
+
+function multiply(a, b) { 
+    return a * b;  // Se agrega 'return' para devolver el resultado
+  }
