cfef3382b8bb061680097c5f3e529401997fbbec
Lorena Febbraro <lorenafebbraro@gmail.com>
2024-11-06 14:07:05 -0300

    kata Elimina espacios de la cadena

---
+++ eliminaEspacios.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+// función que elimine los espacios de una cadena y retorne la cadena con el resultado
+
+//Solucion: replace(/\s+/g, ''): Utiliza una expresión (\s+) para encontrar uno o más espacios en la
+// cadena str y los reemplaza con una cadena vacía (''), eliminando así todos los espacios. 
+//El modificador g asegura que se realice el reemplazo en toda la cadena.
+
+
+
+function removeSpaces(str) {
+    return str.replace(/\s+/g, '');
+}
